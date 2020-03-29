0840f8e8935ef72c3b8e0b3a5e9c3e2e4b0f590e
Julian <juliankimmings@gmail.com>
2020-03-29 18:06:50 +0100

    move data out of store

---
+++ src/store/courses.js
@@ -0,0 +1,37 @@
+export const UNLIMITED = 'Unlimited';
+export default [{ 
+    id: 1,
+    title: "Law",
+    description: "A Law Course",
+    cost: 1000,
+    type: "Online",
+    seats: UNLIMITED
+  },
+  {
+    id: 2,
+    title: "Accounting",
+    description: "An Accounting Course",
+    cost: 500,
+    type: "Classroom",
+    seats: 10,
+    attendance: 1
+  },
+  {
+    id: 3,
+    title: "Management",
+    description: "A Management Course", 
+    cost: 200,
+    type: "Classroom",
+    seats: 10,
+    attendance: 9
+  },
+  {
+    id: 4,
+    title: 'Software Engineering',
+    description: 'A very popular course',
+    cost: 300,
+    type: "Classroom",
+    seats: 10,
+    attendance: 10
+  }
+];
