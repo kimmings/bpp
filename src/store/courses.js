71370da11302ff461fb095bde38f160cb33a1428
Julian <juliankimmings@gmail.com>
2020-03-30 11:17:17 +0100

    update courses to be object for easier lookup

--- src/store/courses.js
+++ src/store/courses.js
@@ -1,37 +1,41 @@
 export const UNLIMITED = 'Unlimited';
-export default [{ 
+
+/* values would be pulled from api via fetch */
+export default {
+  1: { 
     id: 1,
     title: "Law",
     description: "A Law Course",
     cost: 1000,
     type: "Online",
-    seats: UNLIMITED
+    seats: UNLIMITED,
+    attendees: []
   },
-  {
+  2: {
     id: 2,
     title: "Accounting",
     description: "An Accounting Course",
     cost: 500,
     type: "Classroom",
     seats: 10,
-    attendance: 1
+    attendees: [1]
   },
-  {
+  3: {
     id: 3,
     title: "Management",
     description: "A Management Course", 
     cost: 200,
     type: "Classroom",
     seats: 10,
-    attendance: 9
+    attendees: [1, 2, 3, 4, 5, 6, 7, 8, 9]
   },
-  {
+  4: {
     id: 4,
     title: 'Software Engineering',
     description: 'A very popular course',
     cost: 300,
     type: "Classroom",
     seats: 10,
-    attendance: 10
+    attendees: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   }
-];
+};
